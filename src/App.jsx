8467e46ad0649971f5cc267d78ec0ad533ddc8ab
marco-gulino <marcogulino98@gmail.com>
2025-03-11 15:58:27 +0100

    [feat] create routes and import them in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import './App.css'
+// IMPORT Pages
+import Homepage from "./pages/Homepage";
+import Movieslist from "./pages/Movieslist";
 
 function App() {
   return (
-    <>
-      <h1>New Project</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={Homepage} />
+        <Route path="/movies" Component={Movieslist} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Movieslist.jsx
@@ -0,0 +1,7 @@
+export default function Movieslist() {
+  return (
+    <div>
+      <h2>Movies List</h2>
+    </div>
+  );
+}
