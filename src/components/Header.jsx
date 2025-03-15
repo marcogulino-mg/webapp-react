fe29e51fbb6b02d61e2a17357bb46a25543d7ecb
marco-gulino <marcogulino98@gmail.com>
2025-03-15 16:55:28 +0100

    [feat] add two buttons in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,22 @@
+import {Link} from "react-router-dom";
 export default function Header() {
   return (
     <header>
-      <h1>Bool Books</h1>
+      <Link className="link" to="/"><h1 className="mb-0">Bool Books</h1></Link>
+      <nav>
+          <ul className="mb-0">
+              <li>
+                  <Link to="/movies/addmovie">
+                      <button type="button" className="btn btn-primary">Add Movie</button>
+                  </Link>
+              </li>
+              <li>
+                  <Link to="/movies">
+                      <button type="button" className="btn btn-primary">Movies</button>
+                  </Link>
+              </li>
+          </ul>
+      </nav>
     </header>
   );
 }
